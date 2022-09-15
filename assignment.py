95c2ab321112b8e79c1332e9aa5ec4d6500e4b82
Poojashree A M <ampoojashree18@example.com>
2022-09-15 17:06:04 +0530

    commiting chnages

---
+++ assignment.py
@@ -0,0 +1,10 @@
+list1= [1,2,3,2,1]
+list2= [1,3,2,2,1]
+list1.sort()
+list2.sort()
+print(list1)
+print(list2)
+if list1 == list2:
+    print ("The lists are identical")
+else :
+    print ("The lists are not identical")
